3220d977a87487176130d9832d01cf9f55f2bc10
Damian-Oswald <damian.oswald@protonmail.com>
2023-10-03 21:52:51 +0200

    some additional changes

--- process-rate-estimator.R
+++ process-rate-estimator.R
@@ -3,7 +3,7 @@
 #' date: 2023-10-02
 
 #' Define global variables
-colors_variety <- viridis::cividis(4, begin = 0.3, end = 0.8)
+colors_variety <- viridis::mako(4)
 
 data <- read.csv("data/three-way-data.csv", row.names = 1)
 
@@ -28,8 +28,11 @@ boxplot(moisture ~ variety, data)
 boxplot(moisture ~ increment, data) # What is "increment"?
 with(data, interaction.plot(date, depth, moisture)) # Interpretation: the soil moisture starts out relatively homogeneous, but as plants grow, it starts to differ by depth
 for (y in colnames(data)[8:21]) {
-   log <- ifelse(min(data[,y],na.rm=TRUE)>0,"y","")
-   boxplot(data[,y] ~ depth * variety, data, log = log, outline = FALSE, col = rep(colors_variety,each=5),ylab=y, names = rep(sort(unique(data$depth)),4), las = 2)
+   log <- ifelse(min(data[,y],na.rm=TRUE)>0,"x","")
+   boxplot(data[,y] ~ variety * I(-depth), data, log = log, outline = FALSE,
+           col = rep(colors_variety,times=5),xlab=y,
+           names = -rep(sort(unique(data$depth),decreasing = T),each=4),
+           horizontal = TRUE)
 }
 for (y in colnames(data)[8:21]) {
    log <- ifelse(min(data[,y],na.rm=TRUE)>0,"y","")
