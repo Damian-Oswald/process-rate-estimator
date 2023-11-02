924254f3a67106b44ef39c4b3725d7bdd3e12a82
Damian-Oswald <damian.oswald@protonmail.com>
2023-11-02 17:38:50 +0100

    minor changes

--- PRE/R/stateFunctions.R
+++ PRE/R/stateFunctions.R
@@ -1,8 +1,8 @@
 #' @title State function set
 #' 
 #' @export
-stateFunctions <- function(x, e = eps) {
-   f <- numeric(length(x))
+stateFunctions <- function(x, e = getEpsilons(), Ftopin, Fbottomin, Fout, dN2O_dt, SPtopin, SP, SPbottomin, N2O, dSP_dt, d18Otopin, d18Obottomin, d18O, d18O_dt) {
+   f <- numeric(3)
    f[1] <- Ftopin + Fbottomin - Fout + x[1] + x[2] - x[3] - dN2O_dt
    f[2] <- (Ftopin*(SPtopin - e[3] - SP) + Fbottomin*(SPbottomin - e[3] - SP) + x[1]*(e[1]-SP) + 
                x[2]*(e[2]-SP)-(e[3]*Fout + e[4]*x[3]))/N2O - dSP_dt
