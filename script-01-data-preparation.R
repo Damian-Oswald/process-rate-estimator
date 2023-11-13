6ed466d32fb8f229aac87631f3dbd9a713e93f99
Damian-Oswald <damian.oswald@protonmail.com>
2023-11-13 19:30:24 +0100

    minor changes

--- script-01-data-preparation.R
+++ script-01-data-preparation.R
@@ -37,6 +37,8 @@ data[,"N2ONvolume"] <- with(data, N2O * 1/(R*temperature)*28)
 #' Calculate the area N2O-N
 data[,"N2ONarea"] <- with(data, N2ONvolume * increment/100 * (theta_t - moisture) * 10000/1000)
 
+visdat::vis_miss(data[complete,-c(1:13)])
+
 #' -----------------------------------------------------------------------------------------------------------
 #' Start calculating the parameters
 #' -----------------------------------------------------------------------------------------------------------
@@ -60,6 +62,8 @@ data$theta_a <- with(data, 1 - theta_w/theta_t)
 #' Calculate Ds
 data$Ds <- with(data, ((theta_w^(10/3)*D_fw)/H+theta_a^(10/3)*D_fa)*theta_t^-2)
 
+visdat::vis_miss(data[complete,-c(1:13)])
+
 #' *Calculate dC/dz*
 #' In this step, we calculate the N2O concentration gradient as the difference in concentration between the current depth increment and the one above
 for (i in complete) {
@@ -82,26 +86,32 @@ for (i in complete) {
 #' Calculate `F_calc` across depths, considering flux is upward (originally in mg N2O-N/m2/s, converted to g N/ha/day)
 data[,"F_calc"] <- with(data, dCdZ * Ds * rho * 10000 * 3600 * 24/1000)
 
+#' Check missing values
+visdat::vis_miss(data[complete,-c(1:13)])
+data[complete,][is.na(data[complete,"F_bottom_in"]),]
+#' Missing values for column 6 in the lowest depth due to missing increment measurements...
+
 #' Calculate F_in and F_out from the bottom and top boundary of each layer
 for (i in complete) {
    
    # Create a `mask`, which is a logical vector selecting all current 
    j <- which(depths==data[i,"depth"]) # Get the current depth index
-   mask <- with(data, column == data[i,"column"] & date == data[i,"date"] & depth %in% depths[(j-1):j])
+
+   # Save indices of the current increment (center), the one on top and the one on bottom
+   top <- which(with(data, column == data[i,"column"] & date == data[i,"date"] & depth == depths[j-1]))
+   center <- which(with(data, column == data[i,"column"] & date == data[i,"date"] & depth == depths[j]))
+   bottom <- which(with(data, column == data[i,"column"] & date == data[i,"date"] & depth == depths[j+1]))
    
-   F_calc_top <- data[mask & data[,"depth"] == depths[j], "F_calc"]
-   if(j==5) F_calc_bottom <- 0
-   else F_calc_bottom <- data[mask & data[,"depth"] == depths[j+1], "F_calc"]
+   F_calc_top <- data[center,"F_calc"]
+   if(j==5) F_calc_bottom <- 0 else F_calc_bottom <- data[bottom,"F_calc"]
    if(length(F_calc_top)==0) F_calc_top <- NA
    if(length(F_calc_bottom)==0) F_calc_bottom <- NA
    
-   data[mask & data$depth == depths[j],"F_top_in"] <- ifelse(F_calc_top > 0, 0, abs(F_calc_top))
-   data[mask & data$depth == depths[j],"F_top_out"] <- ifelse(F_calc_top < 0, 0, abs(F_calc_top))
-   data[mask & data$depth == depths[j],"F_bottom_in"] <- ifelse(F_calc_bottom < 0, 0, abs(F_calc_bottom))
-   data[mask & data$depth == depths[j],"F_bottom_out"] <- ifelse(F_calc_bottom > 0, 0, abs(F_calc_bottom))
-   
-   # if(j==5) data[mask & data$depth == depths[j], "SP_bottom_in"] <- 0
-   # else data[mask & data$depth == depths[j], "SP_bottom_in"] <- data[mask & data$depth == depths[j+1], "SP"]
+   data[center,"F_top_in"] <- ifelse(F_calc_top > 0, 0, abs(F_calc_top))
+   data[center,"F_top_out"] <- ifelse(F_calc_top < 0, 0, abs(F_calc_top))
+   data[center,"F_bottom_in"] <- ifelse(F_calc_bottom < 0, 0, abs(F_calc_bottom))
+   data[center,"F_bottom_out"] <- ifelse(F_calc_bottom > 0, 0, abs(F_calc_bottom))
+
 }
 
 #' In n' out
--- script-03-state-functions.R
+++ script-03-state-functions.R
@@ -8,6 +8,7 @@
 #' Prepare work space
 #' -----------------------------------------------------------------------------------------------------------
 
+library(BB)
 library(np)
 
 source("script-01-data-preparation.R")
@@ -21,41 +22,41 @@ remove(denominator, F_calc_bottom, F_calc_top, i, j, mask, value, concentration)
 load("resources/hyperparameters.RData")
 
 #' Function to get the derivation of a kernel regression function
-df <- data[data$column==1 & data$depth==30,]
-visdat::vis_miss(df)
-
-# numerical differentiation example
-f <- function(t) t^2
-t <- seq(-5,5,0.1)
-pracma::fderiv(f, t)
+column <- 1
+depth <- 7.5
+variable <- "N2O"
+bandwidth <- hyperparameters[which(depth%in%depths),column,variable]
+subset <- data[data$column == column & data$depth == depth,]
+date <- subset[,"date"]
+derivatives <- data.frame(date = subset[,"date"])
+for(variable in c("N2O", "SP", "d18O")) derivatives[,paste0(variable,"_derivative")] <- getDerivative(x = as.numeric(date), subset[,variable], bandwidth)
 
 #' -----------------------------------------------------------------------------------------------------------
 #' Run solver
 #' -----------------------------------------------------------------------------------------------------------
 
-library(BB)
+data[data$column==column & data$depth==depth & data$date=="2015-09-02",]
+derivatives[derivatives$date=="2015-09-02",2:4]
 
-attach(data[complete[1],])
 
-dN2O_dt <- 1 # get this from the approximated function
-dSP_dt <- 1 # get this from the approximated function
-d18O_dt <- 1 # get this from the approximated function
 
-eqset <- function(x = c(N2Onit = NA, N2Oden = NA, N2Ored = NA), e = unlist(getEpsilons())) {
-   f <- numeric(length(x))
-   f[1] <- F_top_in + F_bottom_in - F_out + x[1] + x[2] - x[3] - dN2O_dt
-   f[2] <- (F_top_in*(SP_top - e[3] - SP) + F_bottom_in*(SP_bottom - e[3] - SP) + x[1]*(e[1]-SP) + 
-               x[2]*(e[2]-SP)-(e[3]*F_out + e[4]*x[3]))/N2O - dSP_dt
-   f[3] <- (F_top_in*(d18O_top - e[7] - d18O) + F_bottom_in*(d18O_bottom - e[7] - d18O) + 
-               x[1]*(e[5]-d18O) + x[2]*(e[6]-d18O)-(e[7]*F_out + e[8]*x[3]))/N2O - d18O_dt
-   return(f)
-}
+stateEquations(
+   x = c(N2Onit = 1, N2Oden = 2, N2Ored = 3),
+   e = unlist(getEpsilons()),
+   derivatives = as.list(derivatives[derivatives$date=="2015-09-02",2:4]),
+   fluxes = as.list(data[data$column==column & data$depth==depth & data$date=="2015-09-02",])
+)
 
-par <- multiStart(par = matrix(runif(1000*3, 0, 40), ncol = 3), fn = eqset, action = "solve", method = 2, 
+par <- multiStart(par = matrix(runif(500*3, 0, 40), ncol = 3),
+                  fn = stateEquations,
+                  action = "solve",
+                  method = 2, 
                   control = list(tol = 5, noimp = 100, maxit = 1500),
                   details = FALSE,
                   quiet = TRUE,
-                  e = unlist(getEpsilons()))
+                  e = unlist(getEpsilons()),
+                  derivatives = as.list(derivatives[derivatives$date=="2015-09-02",2:4]),
+                  fluxes = as.list(data[data$column==column & data$depth==depth & data$date=="2015-09-02",]))
 
 barplot(apply(par$par, 2, mean))
 
