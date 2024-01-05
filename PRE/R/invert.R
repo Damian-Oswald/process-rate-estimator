72dc6ccc11e86be4f3416a46a4a5fbdc6de785ea
Damian-Oswald <damian.oswald@protonmail.com>
2024-01-05 16:41:57 +0100

    Remove R package from repository
    The source code from the R package PRE is moved to another repository to make installing the package easier.

--- PRE/R/invert.R
+++
@@ -1,8 +0,0 @@
-#' @title Invert logical vectors
-#' 
-#' @description This is the equivalent funciton to `!`, but it can be called with the pipe operator.
-#' 
-#' @param x A logical vector.
-#' 
-#' @export
-invert <- function(x) !x
