6764053ef467393171d3bf018ee9b5b13e000659
Damian-Oswald <damian.oswald@protonmail.com>
2024-02-06 20:05:41 +0100

    Work on #4
    - Include ANOVA results in "results" section

--- scripts/results-analysis/results-analysis.R
+++ scripts/results-analysis/results-analysis.R
@@ -1,4 +1,13 @@
 
-data <- read.csv(file.path("results","totalResults.csv"))
+data <- read.csv(file.path("scripts","run-process-rate-estimator","output","estimated-process-rates.csv"))
 
-df <- with(data, tapply(Nitrification_50., list(depth, variety), function(x) paste(signif(mean(x),3),"±",signif(sd(x),3))))
+df <- with(data, tapply(Nitrification, list(depth, variety), function(x) paste(signif(mean(x),3),"±",signif(sd(x),3))))
+
+df <- reshape2::melt(with(data, tapply(Nitrification, list(depth, column), mean)), varnames = c("depth", "column"))
+lm(value ~ depth * as.factor(column), df) |> anova()
+
+df <- reshape2::melt(with(data, tapply(Denitrification, list(depth, column), mean)), varnames = c("depth", "column"))
+lm(value ~ depth, df) |> anova()
+
+df <- reshape2::melt(with(data, tapply(Reduction, list(depth, column), mean)), varnames = c("depth", "column"))
+lm(value ~ depth * as.factor(column), df) |> anova()
