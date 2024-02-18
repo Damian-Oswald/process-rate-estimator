df7d2ca76c75c178c4c325613ce7c85374938188
Damian-Oswald <damian.oswald@protonmail.com>
2024-02-18 18:39:43 +0100

    Work on #7
    - Add cool color gradient to highlight large coefficients

--- scripts/sensitivity-analysis/03-linear-mixed-effects-model.R
+++ scripts/sensitivity-analysis/03-linear-mixed-effects-model.R
@@ -69,35 +69,49 @@ for (d in getParameters()$depth) {
 # ============
 
 # Coefficients
-svg(file.path("scripts","sensitivity-analysis","output","Coefficients.svg"), width = 10, height = 5)
-par(mar = c(2,4,2,0)+0.1)
+svg(file.path("scripts","sensitivity-analysis","output","Coefficients.svg"), width = 12, height = 6)
+par(mar = c(2,4,1,0)+0.1)
 positions <- rep(5*(0:7), each = 3) + rep(1:3, 8)
 centers <- 0.5*(positions[c(diff(positions)==3, FALSE)] + positions[c(FALSE, diff(positions)==3)])
-boxplot(Coefficient ~ Process * Parameter, outline = FALSE, results, xlab = "", ylab = "Standardized Regression Coefficients", boxwex = 0.9, lty = 1, at = positions, col = viridis::cividis(3, begin = 0.2, end = 0.8, alpha = 0.8), cex = 0.5, pch = 16, xaxs = "i", xlim = range(positions), axes = FALSE)
+b <- boxplot(Coefficient ~ Process * Parameter, outline = FALSE, results, xlab = "", ylab = "Standardized Regression Coefficients", boxwex = 0.9, lty = 1, at = positions, col = "transparent", cex = 0.5, pch = 16, xaxs = "i", xlim = range(positions), axes = FALSE)
 abline(v = centers)
 abline(h = seq(-2,3,0.25), lty = 3, lwd = 0.5)
 abline(h = 0)
 axis(2, las = 1)
 box()
-boxplot(Coefficient ~ Process * Parameter, outline = FALSE, results, xlab = "", boxwex = 0.9, lty = 1, at = positions, col = viridis::cividis(3, begin = 0.2, end = 0.8, alpha = 0.8), cex = 0.5, pch = 16, xaxs = "i", xlim = range(positions), axes = FALSE, yaxs = "i", ylim = c(-1, 1), add = TRUE)
+for (i in 1:3) {
+    for (j in 1:8) {
+        x <- subset(results, Process==processes[i]&Parameter==parameters[j])[,"Coefficient"]
+        col <- colorRampPalette(c("lightgrey","red"))(256)
+        points(x = rep(positions[j*3+i-3], length(x))+runif(length(x), -0.25, 0.25), y = x, cex = 0.6, pch = 16, col = col[ceiling(abs(x)*255/2.5)])
+    }
+}
+boxplot(Coefficient ~ Process * Parameter, outline = FALSE, results, xlab = "", boxwex = 0.9, lty = 1, at = positions, col = "transparent", cex = 0.5, pch = 16, xaxs = "i", xlim = range(positions), axes = FALSE, yaxs = "i", ylim = c(-1, 1), add = TRUE)
+text(positions[1:3]+0.4, b$stats[1,1:3]-0.04, processes, srt = 90, pos = 2)
 sapply(1:8, function(i) text(positions[(1:8)*3-1][i], par()$usr[3], expressions[[i]], xpd = NA, pos = 1))
-legend("top", horiz = TRUE, fill = viridis::cividis(3, begin = 0.2, end = 0.8, alpha = 0.8), legend = c("Nitrification", "Denitrification", "Reduction"), inset = c(0, -0.1), xpd = NA, bty = "n")
 dev.off()
 
 # Standardized regression coefficients
-svg(file.path("scripts","sensitivity-analysis","output","SRC.svg"), width = 10, height = 5)
-par(mar = c(2,4,2,0)+0.1)
+svg(file.path("scripts","sensitivity-analysis","output","SRC.svg"), width = 12, height = 6)
+par(mar = c(2,4,1,0)+0.1)
 positions <- rep(5*(0:7), each = 3) + rep(1:3, 8)
 centers <- 0.5*(positions[c(diff(positions)==3, FALSE)] + positions[c(FALSE, diff(positions)==3)])
-boxplot(SRC ~ Process * Parameter, outline = FALSE, results, xlab = "", ylab = "Standardized Regression Coefficients", boxwex = 0.9, lty = 1, at = positions, col = viridis::cividis(3, begin = 0.2, end = 0.8, alpha = 0.8), cex = 0.5, pch = 16, xaxs = "i", xlim = range(positions), axes = FALSE, yaxs = "i", ylim = c(-1, 1))
+b <- boxplot(SRC ~ Process * Parameter, outline = FALSE, results, xlab = "", ylab = "Standardized Regression Coefficients", boxwex = 0.9, lty = 1, at = positions, col = "transparent", cex = 0.5, pch = 16, xaxs = "i", xlim = range(positions), axes = FALSE, yaxs = "i", ylim = c(-1, 1))
 abline(v = centers)
 abline(h = seq(-1,1,0.1), lty = 3, lwd = 0.5)
 abline(h = 0)
 axis(2, las = 1)
 box()
-boxplot(SRC ~ Process * Parameter, outline = FALSE, results, xlab = "", ylab = "Standardized Regression Coefficients", boxwex = 0.9, lty = 1, at = positions, col = viridis::cividis(3, begin = 0.2, end = 0.8, alpha = 0.8), cex = 0.5, pch = 16, xaxs = "i", xlim = range(positions), axes = FALSE, yaxs = "i", ylim = c(-1, 1), add = TRUE)
+for (i in 1:3) {
+    for (j in 1:8) {
+        x <- subset(results, Process==processes[i]&Parameter==parameters[j])[,"SRC"]
+        col <- colorRampPalette(c("lightgrey","red"))(256)
+        points(x = rep(positions[j*3+i-3], length(x))+runif(length(x), -0.25, 0.25), y = x, cex = 0.6, pch = 16, col = col[ceiling(abs(x)*255)])
+    }
+}
+boxplot(SRC ~ Process * Parameter, outline = FALSE, results, xlab = "", boxwex = 0.9, lty = 1, at = positions, col = "transparent", cex = 0.5, pch = 16, xaxs = "i", xlim = range(positions), axes = FALSE, yaxs = "i", ylim = c(-1, 1), add = TRUE)
+text(positions[1:3]+0.4, b$stats[1,1:3]-0.02, processes, srt = 90, pos = 2)
 sapply(1:8, function(i) text(positions[(1:8)*3-1][i], par()$usr[3], expressions[[i]], xpd = NA, pos = 1))
-legend("top", horiz = TRUE, fill = viridis::cividis(3, begin = 0.2, end = 0.8, alpha = 0.8), legend = c("Nitrification", "Denitrification", "Reduction"), inset = c(0, -0.1), xpd = NA, bty = "n")
 dev.off()
 
 
@@ -117,39 +131,39 @@ sapply(1:12, function(c) sapply(getParameters()$depth, function(d) f(c,d,"Reduct
 # FIT LINEAR MIXED EFFECTS MODEL
 # ==============================
 
-paste(parameters, collapse = " + ")
-
-model_nit = lme4::lmer(Nitrification ~ eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction + (eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction || column:depth), data = data)
-model_den = lme4::lmer(Denitrification ~ eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction + (eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction || column:depth), data = data)
-model_red = lme4::lmer(Reduction ~ eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction + (eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction || column:depth), data = data)
-
-model <- tab_model(model_nit, model_den, model_red,
-                   show.ci = FALSE, show.p = TRUE, p.style = "numeric_stars",
-                   terms = parameters)
-
-
-save(model, file = file.path("scripts","sensitivity-analysis","output","lmer.RData"))
-
-sink(file = file.path("resources", "tbl-sensitivity.txt"))
-cat(model$knitr)
-sink()
-
-df <- as.data.frame(VarCorr(model_nit))[-1,c(2,4)]
-barplot(df[,2], names.arg = c(df[-9,1],"Residuals"), las = 2)
-dotchart(df[,2], labels = c(df[-9,1],"Residuals"), log = "x")
-
-
-
-
-model_nit = lme4::lmer(Nitrification ~ eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction +
-                           (eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction | column:depth),
-                       data = data,
-                       REML = TRUE,
-                       verbose = 1,
-                       #subset = sample(1:nrow(data),1000),
-                       control = lmerControl(
-                           optimizer = "Nelder_Mead",
-                           optCtrl = list(iprint = 1000, maxfun = 10000, xst = 0.1)
-                       ))
-tab_model(model_nit)
+# paste(parameters, collapse = " + ")
+# 
+# model_nit = lme4::lmer(Nitrification ~ eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction + (eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction || column:depth), data = data)
+# model_den = lme4::lmer(Denitrification ~ eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction + (eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction || column:depth), data = data)
+# model_red = lme4::lmer(Reduction ~ eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction + (eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction || column:depth), data = data)
+# 
+# model <- tab_model(model_nit, model_den, model_red,
+#                    show.ci = FALSE, show.p = TRUE, p.style = "numeric_stars",
+#                    terms = parameters)
+# 
+# 
+# save(model, file = file.path("scripts","sensitivity-analysis","output","lmer.RData"))
+# 
+# sink(file = file.path("resources", "tbl-sensitivity.txt"))
+# cat(model$knitr)
+# sink()
+# 
+# df <- as.data.frame(VarCorr(model_nit))[-1,c(2,4)]
+# barplot(df[,2], names.arg = c(df[-9,1],"Residuals"), las = 2)
+# dotchart(df[,2], labels = c(df[-9,1],"Residuals"), log = "x")
+# 
+# 
+# 
+# 
+# model_nit = lme4::lmer(Nitrification ~ eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction +
+#                            (eta_SP_diffusion + eta_18O_diffusion + SP_nitrification + d18O_nitrification + SP_denitrification + d18O_denitrification + eta_SP_reduction + eta_18O_reduction | column:depth),
+#                        data = data,
+#                        REML = TRUE,
+#                        verbose = 1,
+#                        #subset = sample(1:nrow(data),1000),
+#                        control = lmerControl(
+#                            optimizer = "Nelder_Mead",
+#                            optCtrl = list(iprint = 1000, maxfun = 10000, xst = 0.1)
+#                        ))
+# tab_model(model_nit)
 
