f22748d140bff789983e8498d74dea3f3e42a4d6
Damian-Oswald <damian.oswald@protonmail.com>
2024-08-11 16:34:46 +0200

    Remove unneccesary files :file_folder:

--- scripts/results-analysis/results-analysis.R
+++ scripts/results-analysis/results-analysis.R
@@ -1,11 +1,11 @@
+# ---
+# title: Analysis of the PRE results
+# ---
 
+# Read data
 data <- read.csv(file.path("scripts","run-process-rate-estimator","output","estimated-process-rates.csv"))
 
-df <- with(data, tapply(Nitrification, list(depth, variety), function(x) paste(signif(mean(x),3),"±",signif(sd(x),3))))
-
-
-
-with(data, table(column, variety))
+# create factor variable for variety
 variety <- factor(c("Zinal", "Probus", "CH Claro", "Monte Calme 268", "Probus", "CH Claro", "Monte Calme 268", "CH Claro", "Probus", "Zinal", "Zinal", "Monte Calme 268"))
 
 # Nitrification
@@ -23,12 +23,4 @@ lm(value ~ variety, df) |> anova()
 # Reduction
 df <- reshape2::melt(with(data, tapply(Reduction, list(depth, column), mean)), varnames = c("depth", "column"))
 df$variety <- variety[df$column]
-with(df, interaction.plot(depth, variety, value))
 lm(value ~ depth * as.factor(column), df) |> anova()
-
-boxplot(value ~ variety, df, col = "transparent")
-lm(value ~ variety, df) |> anova()
-
-# Effect of variety
-lm(value ~ variety, df) |> anova()
-
